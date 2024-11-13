1faedbd24c8f4279326c1df123326bcf22ddf3d4
Nalla2004 <allan.cruz.202203845@gmail.com>
2024-11-12 19:59:25 -0600

    routes de admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -53,8 +53,7 @@ $routes->get('/contacto', 'MenuController::contacto');
 $routes->get('/novedades', 'MenuController::novedades');
 
 // Ruta para la vista de administrador
-$routes->get('/vista_admin', 'VistaAdminController::index');
-
+$routes->get('/admin', 'VistaAdminController::index');
 
 // Rutas para Proveedores
     $routes->get('proveedores', 'ProveedoresController::index');
