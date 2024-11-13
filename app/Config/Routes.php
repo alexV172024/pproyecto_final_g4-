143af15557ac869f0e8a7ad8865d8ea822b82bf3
Nalla2004 <allan.cruz.202203845@gmail.com>
2024-11-12 21:23:00 -0600

    vista admin parches

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -58,7 +58,7 @@ $routes->get('/contacto', 'MenuController::contacto');
 $routes->get('/novedades', 'MenuController::novedades');
 
 // Ruta para la vista de administrador
-$routes->get('/admin', 'VistaAdminController::index');
+$routes->get('/admin', 'AdminController::vista_admin');
 
 // Rutas para Proveedores
     $routes->get('proveedores', 'ProveedoresController::index');
