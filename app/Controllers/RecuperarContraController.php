6db2de2f50dc22faa9eab7d4e9d6db56c74b1fbf
Nalla2004 <allan.cruz.202203845@gmail.com>
2024-11-13 18:05:50 -0600

    controller de contra

---
+++ app/Controllers/RecuperarContraController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class RecuperarContraController extends Controller
+{
+    public function index()
+    {
+        return view('recuperarcontra/index');
+    }
+}
